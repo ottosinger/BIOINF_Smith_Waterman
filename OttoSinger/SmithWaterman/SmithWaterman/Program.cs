a7565828f99eb967db9512295f6e1c43a96841a4
ottosinger <otto.singer@fer.hr>
2018-01-15 10:49:10 +0100

    verifying git ignore for build files - success

--- OttoSinger/SmithWaterman/SmithWaterman/Program.cs
+++ OttoSinger/SmithWaterman/SmithWaterman/Program.cs
@@ -10,6 +10,8 @@ namespace SmithWaterman
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
